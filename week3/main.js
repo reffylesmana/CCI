5328f97e43d5bbad507bbc699e7e4640fa85feb2
YFFER7 <Reffylesmana0@gmail.com>
2026-04-02 20:18:42 +0700

    y

--- week3/main.js
+++ week3/main.js
@@ -1,12 +1,12 @@
-console.log("1. pesan kopi...");
-const start = new Date()
+// console.log("1. pesan kopi...");
+// const start = new Date()
 
-while (new Date() - start < 3000) {
+// while (new Date() - start < 3000) {
 
-}
-console.log("2. pesan sudah siap, silakan dinikmati...");
-console.log("3. pesan roti...");
-console.log("4. roti sudah jadi, silakan dinikmati...")
+// }
+// console.log("2. pesan sudah siap, silakan dinikmati...");
+// console.log("3. pesan roti...");
+// console.log("4. roti sudah jadi, silakan dinikmati...")
 
 // console.log("1. pesan teh...");
 // setTimeout(() => {
@@ -15,3 +15,32 @@ console.log("4. roti sudah jadi, silakan dinikmati...")
 
 // console.log("3. pesan kue...");
 // console.log("4. kue sudah jadi, silakan dinikmati...")
+
+const pesanKopi = new Promise((resolve, reject) => {
+  let stock = true;
+  setTimeout(() => {
+    if (stock) {
+      resolve("pesan sudah siap, silakan dinikmati...");
+    } else {
+      reject("maaf, stok kopi habis...");
+    }
+  }, 3000);
+});
+
+// pesanKopi.then((message) => {
+//   console.log(message).catch((error) => {
+//     console.log(error);
+//   }); 
+// });
+
+async function pesanKopiAsync() {
+  try {
+    console.log("pesan dibuat...");
+    const message = await pesanKopi;
+    console.log(message);
+  } catch (error) {
+    console.log(error);
+  }
+} 
+
+pesanKopiAsync();
