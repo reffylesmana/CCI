29b02b0c31a305e1d665c9e3f53a87aaa817c5bf
YFFER7 <Reffylesmana0@gmail.com>
2026-04-02 20:07:00 +0700

    first commit

---
+++ week3/main.js
@@ -0,0 +1,17 @@
+console.log("1. pesan kopi...");
+const start = new Date()
+
+while (new Date() - start < 3000) {
+
+}
+console.log("2. pesan sudah siap, silakan dinikmati...");
+console.log("3. pesan roti...");
+console.log("4. roti sudah jadi, silakan dinikmati...")
+
+// console.log("1. pesan teh...");
+// setTimeout(() => {
+//     console.log("2. pesan sudah siap, silakan dinikmati...");
+// }, 3000);
+
+// console.log("3. pesan kue...");
+// console.log("4. kue sudah jadi, silakan dinikmati...")
